923efefdc0ab084cf0a88babea28e7a68b4e4af6
Admin <jashim6969@gmail.com>
2021-03-23 18:36:14 +0600

    23/3/2021

--- software/insurance/urls.py
+++ software/insurance/urls.py
@@ -3,7 +3,37 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
+    path('', views.mainV,name='main'),
     path('', views.indexV,name='index'),
+    path('login/', views.logingV,name='login'),
+    path('logout/', views.loguotV,name='logout'),
+    path('Dashboard/', views.DashboardV,name='Dashboard'),
+    path('create/user/', views.createuserV,name='createuser'),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- software/insurance/views.py
+++ software/insurance/views.py
@@ -1,6 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import auth,messages
+from django.core.files.storage import FileSystemStorage
+from .models import *
 
 # Create your views here.
 
 def indexV(request):
-    return render(request,'index.html')
+    a=UserProfileM.objects.all()
+    return render(request,'index.html',{'a':a})
+
+def mainV(request):
+    return render(request,'pages/main.html')
+
+
+def logingV(request):
+    if request.method=='POST':
+        name=request.POST.get('name')
+        password=request.POST.get('password')
+        user = auth.authenticate(username=name, password=password)
+        if user is not None:
+            auth.login(request, user)
+            if 'next' in request.POST:
+                # return redirect(request.POST.get('next'))
+                return redirect('/')
+            else:
+                return redirect('/')
+        else:
+            messages.info(request, 'User is not valid')
+            return redirect('/login/')
+    else:
+        return render(request,'pages/login.html')
+
+def loguotV(request):
+    auth.logout(request)
+    return redirect('/login/')
+
+def createuserV(request):
+    if request.method == 'POST' and request.FILES:
+        username = request.POST.get('username')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        present = request.POST.get('present')
+        permanent = request.POST.get('permanent')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'User Name already taken')
+                return redirect('/Singup/')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'E-Mail already taken')
+                return redirect('/Singup/')
+            else:
+                image = request.FILES['image']
+                store = FileSystemStorage()
+                filename = store.save(image.name, image)
+                profile_pic_url = store.url(filename)
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+                sing = UserProfileM(user=user,Phone=phone, Present_Address=present, Permanant_Address=permanent, Image=filename)
+                sing.save()
+                messages.info(request, 'Data Saved')
+                return redirect('/create/user/')
+        else:
+            messages.info(request, 'password donot match')
+            return redirect('/create/user/')
+    else:
+        return render(request,'admin/usercreate.html')
+
+
+def DashboardV(request):
+    return render(request,'pages/Dashboard.html')
+
+
+
